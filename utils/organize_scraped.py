c8d3ebf08295295b8c731897f7e5addb17a6897e
sagun-shakya <sagun.shakya@naamii.org.np>
2022-04-22 07:42:52 +0545

    Changed name of the output file.

--- utils/organize_scraped.py
+++ utils/organize_scraped.py
@@ -17,6 +17,6 @@ dataframe_list = [pd.read_csv(my_path, encoding='utf-8', skip_blank_lines = True
 df_main = pd.concat(dataframe_list, axis = 0, ignore_index = True)
 
 #%% To CSV.
-df_main.to_csv(join(root, 'df_merged_search_terms_21-Apr-2021.csv'), index = None, encoding = 'utf-8')
+df_main.to_csv(join(root, 'df_merged_search_terms_21-Apr-2021_nep.csv'), index = None, encoding = 'utf-8')
 
-df_main.keyword.value_counts().to_csv(join(root, 'tweet_counts.csv'), encoding = 'utf-8', header = None)
+df_main.keyword.value_counts().to_csv(join(root, 'tweet_counts_nep.csv'), encoding = 'utf-8', header = None)
