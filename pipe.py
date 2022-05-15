7b07571e77ea769076f14c78195b129c3fa448f1
sagun-shakya <sagun.shakya@naamii.org.np>
2022-05-15 09:39:05 +0545

    Added docs. Fixed time end position. Fixed kw args

--- pipe.py
+++ pipe.py
@@ -2,7 +2,7 @@
 import os
 from time import sleep, time
 
-from pandas import Series, read_csv
+from pandas import DataFrame, read_csv
 from os.path import join, exists
 
 # Local modules.
@@ -11,24 +11,33 @@ from pipeline.twint_scraper import scrape_tweets
 from utils import utils
 from utils.organize_scraped import organizer
 
-def collect_tweets_from_seed(seed: str):
+def collect_tweets_from_seed(seed: str) -> DataFrame:
+    """
+    Collects tweets based on a search term (or seed) and applies alanguage filter to remove hindi and other devanagiri using script so that only nepali tweets are obtained.
+
+    Args:
+        seed (str): search term or keyword for making the search.
+
+    Returns:
+        Pandas DataFrame: Dataframe containing tweets with the following attributes : {id conversation_id date tweet language hashtags user_id_str username link retweet nreplies search reply_to}
+    """    
     start = time()
     fields = 'id conversation_id date tweet language hashtags user_id_str username link retweet nreplies search reply_to'.split()
     print(f"\nSearch Term : {seed}\n{'_'*50}")
 
     df = scrape_tweets(search_term = seed, fields = fields, store_csv = False)
+    end = time()
     print(f'Collected {len(df)} Tweets in {end - start: .5f} seconds.\n{"-" * 50}')
     
     # Apply language filter to obtain only nepali tweets.
     df = df[df['language'] == 'ne']
 
-    end = time()
-    print(f'Collected {len(df)} filtered Tweets in {end - start: .5f} seconds.\n{"-" * 50}')
+    print(f'Collected {len(df)} filtered Nepali Tweets.\n{"-" * 50}')
 
     return df
 
 
-def main(target_dir:str = r'results/second_lot', output_filename: str = 'scraped_keywords_.xlsx', organize_tweets: bool = True):
+def main(target_dir:str, output_filename: str, organize_tweets: bool = True):
 
     keywords = read_csv('keywords.txt', header = None, encoding = 'utf-8', skip_blank_lines = True)
     keywords = keywords.drop_duplicates(keep = 'first').values.ravel()
@@ -36,7 +45,7 @@ def main(target_dir:str = r'results/second_lot', output_filename: str = 'scraped
 
     os.makedirs(target_dir, exist_ok=True)
 
-    for SEED in keywords:   # Lot 2.
+    for SEED in keywords[:]:   # Lot 2.
 
         save_filename = f'scraped_{SEED}.csv'
         if not exists(join(target_dir, save_filename)):
@@ -52,13 +61,12 @@ def main(target_dir:str = r'results/second_lot', output_filename: str = 'scraped
     
     # Organize scraped Tweets in a file.
     if organize_tweets:
-        organizer(root = target_dir, output_filename = 'scraped_all_keywords_04-05-022.xlsx')
+        organizer(root = target_dir, output_filename = output_filename)
     
 
 if __name__ == "__main__":
-    #df = scrape_tweets(search_term = None, store_csv = True, save_dir = r'results/temp', verbose = True)
     '''
     Driver Code.
     '''
-    main(target_dir = r'results/all_keywords', output_filename = 'scr
+    main(target_dir = r'results/all_keywords', output_filename = 'scraped_all_keywords_15-05-022.xlsx', organize_tweets = True)
     
